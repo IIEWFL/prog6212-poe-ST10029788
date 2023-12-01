be152ab2eb1badcfb7f5d440ba4af0e07b02b33b
ST10029788 <103359783+ST10029788@users.noreply.github.com>
2023-12-01 12:08:18 +0200

    Add files via upload

---
+++ Pages/Modules/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ModuleCalculatorWeb.Data;
+using ModuleCalculatorWeb.Models;
+
+namespace ModuleCalculatorWeb.Pages.Modules
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ModuleCalculatorWeb.Data.ApplicationDbContext _context;
+
+        public CreateModel(ModuleCalculatorWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Module Module { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            // Calculations done by the class library
+            Module.selfStudy = CalculateMod.Class1.SelfStudies(Module.credits, Module.Weeks, Module.hrs);
+            Module.remain  = CalculateMod.Class1.remainingHours(Module.selfStudy, Module.studyHrs);
+            _context.Module.Add(Module);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
